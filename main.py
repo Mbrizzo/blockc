31073ee629cd3f325fd897e4bcedc980f3af0188
Marcel <mrizzo25@gmail.com>
2023-05-24 17:54:44 -0300

    f mine_block

--- main.py
+++ main.py
@@ -14,10 +14,19 @@ class Block:
         sha = hashlib.sha256()
         sha.update((str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash) + str(self.nonce)).encode('utf-8'))
         return sha.hexdigest()
+    
+    #  valor de nonce será acrescido ao bloco quando a condição da mineração seja satisfeita.
+    def mine_block(self, difficulty):
+        target = '0' * difficulty
+        while self.hash[:difficulty] != target:
+            self.nonce += 1
+            self.hash = self.calculate_hash()
+
 
 class Blockchain:
     def __init__(self):
         self.chain = [self.create_genesis_block()]
+        self.difficulty = 4
 
     def create_genesis_block(self):
         return Block(0, time.time(), "Genesis Block", "0")
@@ -27,6 +36,7 @@ class Blockchain:
 
     def add_block(self, new_block):
         new_block.previous_hash = self.get_latest_block().hash
+        new_block.mine_block(self.difficulty)
         new_block.hash = new_block.calculate_hash()
         self.chain.append(new_block)
 
@@ -42,22 +52,30 @@ class Blockchain:
 
 blockchain = Blockchain()
 
-block1 = Block(1, time.time(), "Red", blockchain.get_latest_block().hash)
-block2 = Block(2, time.time(), "Green", blockchain.get_latest_block().hash)
-block3 = Block(3, time.time(), "Blue", blockchain.get_latest_block().hash)
-block4 = Block(4, time.time(), "Black", blockchain.get_latest_block().hash)
-block5 = Block(5, time.time(), "White", blockchain.get_latest_block().hash)
-
+print("Mineração do bloco 1...")
+block1 = Block(1, time.time(), "Red", "")
 blockchain.add_block(block1)
+
+print("Mineração do bloco 2...")
+block2 = Block(2, time.time(), "Green", "")
 blockchain.add_block(block2)
-blockchain.add_block(block3)
-blockchain.add_block(block4)
-blockchain.add_block(block5)
+
+print("Mineração do bloco 3...")
+block1 = Block(1, time.time(), "Blue", "")
+blockchain.add_block(block1)
+
+print("Mineração do bloco 4...")
+block2 = Block(2, time.time(), "Black", "")
+blockchain.add_block(block2)
+
+# Verifica a validade da cadeia
+print("Validade da cadeia:", blockchain.is_chain_valid())
 
 for block in blockchain.chain:
     print(f"Block {block.index}:")
     print(f"Timestamp: {block.timestamp}")
     print(f"Data: {block.data}")
+    print(f"Nonce: {block.nonce}")
     print(f"Previous Hash: {block.previous_hash}")
     print(f"Hash: {block.hash}")
     print()
