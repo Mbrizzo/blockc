a60cb6f1b473351c53286d5fcefa8465c9130871
Marcel <mrizzo25@gmail.com>
2023-05-24 00:59:00 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import hashlib
+import time
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.nonce = 0
+        self.hash = self.calculate_hash()
