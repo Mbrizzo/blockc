4385cb93531536b769a07d43b574759f9aaef1fa
Marcel <mrizzo25@gmail.com>
2023-05-26 03:16:46 -0300

    added sync in main - sync fixed

--- sync.py
+++ sync.py
@@ -1,38 +1,38 @@
 import socket
 import pickle
 
-def synchronize_chain(self, host, port):
-        
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((host, port))
-       
-        request = 'GET_CHAIN'
-        sock.sendall(request.encode())
-       
-        data = b''
-        while True:
-            chunk = sock.recv(4096)
-            if not chunk:
-                break
-            data += chunk
-        
-        self.chain = pickle.loads(data)
+def synchronize_chain(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+   
+    request = 'GET_CHAIN'
+    sock.sendall(request.encode())
+   
+    data = b''
+    while True:
+        chunk = sock.recv(4096)
+        if not chunk:
+            break
+        data += chunk
+    
+    chain = pickle.loads(data)
+    sock.close()
+    
+    return chain
 
-        sock.close()
+def listen_for_requests(port, blockchain):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('localhost', port))
+    sock.listen(1)
 
-def listen_for_requests(self, port):       
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind(('localhost', port))
-        sock.listen(1)
+    while True:
+        conn, addr = sock.accept()
 
-        while True:
-            conn, addr = sock.accept()
+        # Recebimento da solicitação
+        data = conn.recv(1024).decode()
 
-            # Recebimento da solicitação
-            data = conn.recv(1024).decode()
+        if data == 'GET_CHAIN':
+            # Envio da cadeia de blocos em resposta à solicitação
+            conn.sendall(pickle.dumps(blockchain.chain))
 
-            if data == 'GET_CHAIN':
-                # Envio da cadeia de blocos em resposta à solicitação
-                conn.sendall(pickle.dumps(self.chain))
-
-            conn.close()
+        conn.close()
