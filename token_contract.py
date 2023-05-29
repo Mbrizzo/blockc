2aff8a58dabe59752c5016abf0098d83d0cfc553
Marcel <mrizzo25@gmail.com>
2023-05-29 16:51:39 -0300

    token_contract

--- token_contract.py
+++ token_contract.py
@@ -8,8 +8,30 @@ contract_file = open("contract.sol", "r")
 contract_code = contract_file.read()
 contract_file.close()
 
-abi = ''
-bytecode = ''
+# solc --abi --bin contract.sol -o build/
+with open('build/Counter.json', 'r') as f:
+    compiled_contract_data = json.load(f)
 
-compiled_contract = web3.eth.contract(abi=json.loads(abi), bytecode=bytecode)
+abi = compiled_contract_data['abi']
+bytecode = compiled_contract_data['bytecode']
 
+compiled_contract = web3.eth.contract(abi=abi, bytecode=bytecode)
+
+account = web3.eth.accounts[0]  # endereço no Ganache
+
+# Transação de implantação do contrato
+transaction = compiled_contract.constructor().buildTransaction({
+    "from": account,
+    "nonce": web3.eth.getTransactionCount(account),
+    "gas": 4000000,  # Substitua pelo valor adequado de gás
+})
+
+# Assinar e enviar a transação
+signed_txn = web3.eth.account.signTransaction(transaction, private_key="sua_chave_privada")
+transaction_hash = web3.eth.sendRawTransaction(signed_txn.rawTransaction)
+
+# Aguardar a confirmação da transação
+transaction_receipt = web3.eth.waitForTransactionReceipt(transaction_hash)
+
+# Obter o endereço do contrato implantado
+contract_address = transaction_receipt["contractAddress"]
